26515420a5f882b94b76dc98f97ff3ad84718c7e
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-01-26 18:09:45 +0100

    refactor code

--- main.go
+++ main.go
@@ -11,24 +11,27 @@ import (
 func main() {
 	checkLinux()
 	checkRoot()
-	run()
+	switch os.Args[1] {
+	case "run":
+		run()
+	}
 }
 
 func checkLinux() {
 	if runtime.GOOS != "linux" {
-		panic("This program can only run on Linux")
+		panic("this program can only run on Linux")
 	}
 }
 
 func checkRoot() {
 	if os.Geteuid() != 0 {
-		panic("This program can only run as root")
+		panic("this program must be run as root")
 	}
 }
 
 func run() {
-	fmt.Printf("Running as pid: %d\n", os.Getpid())
-	cmd := exec.Command("/bin/bash")
+	fmt.Printf("Running %v as pid: %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
