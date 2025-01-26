1dd8cd75f5c6916d17b0ff3626bd78de93f74919
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-01-26 23:03:58 +0100

    add second program

---
+++ container2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
+)
+
+func main() {
+	checkLinux()
+	checkRoot()
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	}
+}
+
+func checkLinux() {
+	if runtime.GOOS != "linux" {
+		panic("this program can only run on Linux")
+	}
+}
+
+func checkRoot() {
+	if os.Geteuid() != 0 {
+		panic("this program must be run as root")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v as pid: %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as pid: %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	must(syscall.Sethostname([]byte("container")))
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
